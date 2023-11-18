4de143bbee63f493ebcef19096eb901a2ca5f474
mar1nho <gustavomarinhosom@gmail.com>
2023-11-18 00:40:11 -0300

    db connection

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -1,4 +1,65 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
 public class DBConnection {
+	
+	private static Connection connection = null;
+	
+	public static Connection getConnection(){
+		if (connection == null){
+			try {
+				Properties properties = loadProperties();
+				String url = properties.getProperty("dburl");
+				connection = DriverManager.getConnection(url, properties);
+			} catch (SQLException e){
+				throw new DBException(e.getMessage());
+			}
+		}
+		return connection;
+	}
+	
+	private static Properties loadProperties(){
+		try (FileInputStream fs = new FileInputStream("src/main/resources/db.properties")){
+			Properties properties = new Properties();
+			properties.load(fs);
+			return properties;
+		} catch (IOException e){
+			throw new RuntimeException(e.getMessage());
+		}
+	}
+	
+	public static void closeConnection(){
+		if (connection != null){
+			try{
+				connection.close();
+			} catch (SQLException e){
+				throw new DBException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeStatement(Statement statement){
+		if (statement != null){
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				throw new DBException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet resultSet){
+		if (resultSet != null){
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				throw new DBException(e.getMessage());
+			}
+		}
+	}
+	
 }
