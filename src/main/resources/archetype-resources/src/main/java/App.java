6e688fdfb9490e53e28b4a487c5d374c37c6c933
mar1nho <gustavomarinhosom@gmail.com>
2023-11-18 00:29:40 -0300

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $net.m4rinho;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
